70abba82d7b5b14e9fb11cce70fb42a3b3840c50
Jose Ramon Macia Gaitan <joseramon.macia@atlasinformatica.com>
2022-09-08 15:19:13 +0000

    Add Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 import './App.css';
 
 function App() {
+
+  const Title = styled.h1`
+    text-align: center;
+  `;
+
   return (
     <div>
       <header>
-        <h1>Game of Thrones</h1>
+        <Title>Game of Thrones</Title>
       </header>
       
     </div>
