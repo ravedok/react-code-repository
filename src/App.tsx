478a396bdc61cd6c6f29f125c97304d39e6c683a
Jose Ramon Macia Gaitan <joseramon.macia@atlasinformatica.com>
2022-09-11 17:42:17 +0000

    Add Loading image

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import styled from 'styled-components';
-import './App.css';
-import { PostList } from './components/PostList/PostList';
+import React from "react";
+import styled from "styled-components";
+import "./App.css";
+import { PostList } from "./components/PostList/PostList";
 
 export const Title = styled.h1`
-    text-align: center;
-  `;
+  text-align: center;
+`;
 
 function App() {
   return (
     <div>
       <header>
         <Title>Fake Blog</Title>
-        <PostList />
       </header>
+      <PostList />
     </div>
   );
 }
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -3,47 +3,68 @@ import styled from "styled-components";
 import { PaginatedResult } from "../../api/PaginatedResult";
 import { getPosts } from "../../api/posts";
 import { Post } from "../../models/Post";
+import { Loading } from "../Loading/Loading";
 import { Paginator } from "../Paginator/Paginator";
 import { PostListItem } from "./PostListItem";
 
 const List = styled.ul`
-    margin: 0;
-    padding: 0;
-    display: grid;
-    row-gap: 1rem ;
-    column-gap: 1rem;
-    grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+  margin: 0;
+  padding: 0;
+  display: grid;
+  row-gap: 1rem;
+  column-gap: 1rem;
+  grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
 `;
 
 const Container = styled.section`
-    margin: 1rem;
-    display: flex;
-    flex-direction: column;
+  margin: 1rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+const LoadingContainer = styled.div`
+  height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const PostList = () => {
-    
-    const [posts, setPosts] = useState<PaginatedResult<Post>>();
+  const [posts, setPosts] = useState<PaginatedResult<Post>>();
 
-    const handlePageChange = (newPage: number) => {
-        getPosts(newPage).then(setPosts);
-    }
+  const [loading, setLoading] = useState<boolean>(false);
 
+  const loadData = (page: number = 1) => {
+    setLoading(true);
+    getPosts(page).then((posts) => {
+      setPosts(posts);
+      setLoading(false);
+    });
+  };
 
-    useEffect(() => {
-        getPosts().then(setPosts);
-    }, [])
+  const handlePageChange = (newPage: number) => {
+    loadData(newPage);
+  };
 
-    return (    
-        <Container>
-            {posts &&  (
-                <React.Fragment>
-                    <List>
-                        {posts.data.map(post => <PostListItem key={ post.id } post={post} />)}
-                    </List>
-                    <Paginator {...posts} onChangePage={handlePageChange} />
-                </React.Fragment>
-            )}
-        </Container>
-    )
+  useEffect(() => loadData(), []);
+
+  return (
+    <Container>
+      {loading && (
+        <LoadingContainer>
+          <Loading />
+        </LoadingContainer>
+      )}
+      {!loading && posts && (
+        <React.Fragment>
+          <List>
+            {posts.data.map((post) => (
+              <PostListItem key={post.id} post={post} />
+            ))}
+          </List>
+          <Paginator {...posts} onChangePage={handlePageChange} />
+        </React.Fragment>
+      )}
+    </Container>
+  );
 };
