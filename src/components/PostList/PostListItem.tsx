28109ee662daf5f02ede72eaad60c539c4a2c0eb
Jose Ramon Macia Gaitan <joseramon.macia@atlasinformatica.com>
2022-09-14 19:55:29 +0000

    Fix Post List Item anchor

--- src/components/PostList/PostListItem.tsx
+++ src/components/PostList/PostListItem.tsx
@@ -1,59 +1,56 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Post } from '../../models/Post';
+import React from "react";
+import styled from "styled-components";
+import { Post } from "../../models/Post";
 
 type Props = {
-    post: Post
-}
+  post: Post;
+};
 
 const Title = styled.h4`
-    margin: .25rem 0;    
+  margin: 0.25rem 0;
 `;
 
 const User = styled.div`
-    display: flex;
-    gap: .25rem;
+  display: flex;
+  gap: 0.25rem;
+  flex-direction: column;
 `;
 
 const UserEmail = styled.a``;
 const UserWebsite = styled.a`
-    &:before {
-        content: '<';
-    }
-    
-    &:after {
-        content: '>';
-    }
+  &:before {
+    content: "<";
+  }
 
+  &:after {
+    content: ">";
+  }
 `;
 
-
-
 const Body = styled.div`
-    margin-top: 1rem;
+  margin-top: 1rem;
 `;
 
 const Container = styled.li`
-    list-style: none;
-    border: 1px solid #CCC;
-    padding: 1rem;    
-`
+  list-style: none;
+  border: 1px solid #ccc;
+  padding: 1rem;
+`;
 
+export const PostListItem = ({ post }: Props) => {
+  const { title, body, user } = post;
+  const { name: userName, email: userEmail, website: userWebsite } = user;
 
+  const emailHref = `mailto:${userEmail}`;
 
-export const  PostListItem = ({ post }: Props)  =>  {
-
-    const {title, body, user} = post;
-    const {name: userName, email: userEmail, website: userWebsite } = user;
-
-    return (
-        <Container>
-            <Title>{ title }</Title> 
-            <User>
-                <UserEmail href={`mailto:${userEmail}`}>{userName}</UserEmail>
-                <UserWebsite href={UserWebsite}>{userWebsite}</UserWebsite>
-            </User>
-            <Body>{ body }</Body>
-        </Container>
-    );
-}
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <User>
+        <UserEmail href={emailHref}>{userName}</UserEmail>
+        <UserWebsite href={userWebsite}>{userWebsite}</UserWebsite>
+      </User>
+      <Body>{body}</Body>
+    </Container>
+  );
+};
